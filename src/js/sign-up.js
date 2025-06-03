2e7f717a8a21d26daaf4da68411af8d55eeacae9
Raphcel <rafiffarras23@gmail.com>
2025-06-03 14:22:50 +0700

    fix (2)

--- src/js/sign-up.js
+++ src/js/sign-up.js
@@ -59,7 +59,7 @@ signupBtn.addEventListener('click', async () => {
 
   alert('Signup successful! Please check your email to verify your account.')
   setTimeout(() => {
-    window.location.href = '/'
+    window.location.href = 'https://exoticmahseer.netlify.app/index.html'
   }, 2000)
 })
 
